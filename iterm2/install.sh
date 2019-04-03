50b40b9fc297dbbfb7b78de4c342f5fdfcea5682
Karun Japhet <karun@japhet.in>
2019-04-04 00:24:59 +0530

    Add user readable installation messages for iterm2 config

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,2 +1,8 @@
+# `default writes` are not used because the iterm2 data structure is painful to setup
+# Hopefully this changes in the future. The approach below is currently iterm2's recommended setup procedure
+echo
+echo "Deleting iterm2 settings"
 defaults delete com.googlecode.iterm2
+
+echo "Replacing with checked in iterm2 settings"
 cp com.googlecode.iterm2.plist /Library/Preferences/
