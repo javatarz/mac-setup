3c10be7b3896f1873a17cda2399cfda643325af0
Karun Japhet <karun@japhet.in>
2019-04-16 10:38:09 +0530

    Fix the datatype for dock minimize-to-application

--- osx/menu-and-dock.sh
+++ osx/menu-and-dock.sh
@@ -8,7 +8,7 @@ defaults write com.apple.dock tilesize -float 32
 defaults write com.apple.dock largesize -float 92
 defaults write com.apple.dock orientation right
 defaults write com.apple.dock mineffect genie
-defaults write com.apple.dock minimize-to-application 1
+defaults write com.apple.dock minimize-to-application true
 delete_if_available com.apple.dock persistent-apps
 defaults write com.apple.dock show-recents -bool false
 echo ">> Restarting Dock"
