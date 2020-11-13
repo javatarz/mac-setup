5adc645364866eca8266be4052ca02b3b6c2c964
Karun Japhet <karun@japhet.in>
2020-11-13 11:36:25 +0530

    Updates

--- shell/git.fish
+++ shell/git.fish
@@ -1,37 +1,50 @@
 function gs
-  git status
+    git status
 end
 
 function gd
-  git diff $argv
+    git diff $argv
 end
 
 function ga
-  git add $argv
+    git add $argv
 end
 
 function gc
-  git commit $argv
+    git commit $argv
 end
 
 function pull
-  git pull -r
+    git pull -r
 end
 
 function push
-  git push
+    git push
 end
 
 function reset
-  git reset --hard HEAD
+    git reset --hard HEAD
+end
+
+function stash
+    git stash $argv
+end
+
+function pop
+    git stash pop $argv
 end
 
 function signpersonal
-  git config user.signingkey $GPG_KEY_PERSONAL
-  git config user.email "$GPG_MAIL_PERSONAL"
+    git config user.signingkey $GPG_KEY_PERSONAL
+    git config user.email "$GPG_MAIL_PERSONAL"
 end
 
 function signwork
-  git config user.signingkey $GPG_KEY_WORK
-  git config user.email "$GPG_MAIL_WORK"
+    git config user.signingkey $GPG_KEY_WORK
+    git config user.email "$GPG_MAIL_WORK"
+end
+
+function fuck_it
+    set commit_message (curl -s http://whatthecommit.com/index.txt)
+    git commit -m "$commit_message"
 end
--- shell/lazy.fish
+++ shell/lazy.fish
@@ -1,15 +1,44 @@
 function awsv
-  aws-vault --prompt=terminal $argv
+    aws-vault --prompt=terminal $argv
 end
 
 function json
-  cat $argv[1] | jq .
+    cat $argv[1] | jq .
 end
 
 function dc
-  docker-compose $argv
+    docker-compose $argv
 end
 
 function weather
-  curl "http://v2.wttr.in/$argv[1]"
+    curl "http://v2.wttr.in/$argv[1]"
+end
+
+function coffee
+    brew update
+    brew upgrade
+    brew outdated --cask | cut -f 1 -d " " | xargs brew cask reinstall
+    brew cleanup
+    mas upgrade
+    softwareupdate --all --install --force
+end
+
+function notebook
+    set command $argv[1]
+    set notebook_root $argv[2]
+    set -q command[1]; or set command start
+    set -q notebook_root[1]; or set notebook_root "$HOME/projects/personal/notebook"
+
+    if test "$command" = "start"
+        echo "mapping work to $notebook_root"
+        docker run -p 8888:8888 -d -P --name notebook -v $notebook_root:/home/jovyan/work jupyter/datascience-notebook
+        docker logs notebook
+    else if test "$command" = "stop"
+        docker stop notebook
+        docker rm notebook
+    else if test "$command" = "logs" || test "$command" = "log"
+        docker logs notebook
+    else
+        echo "Invalid command: '$command'"
+    end
 end
