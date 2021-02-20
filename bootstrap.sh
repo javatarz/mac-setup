05335d21d4c6af9105a4a3e10f21c8884c2b518a
Karun Japhet <karun@japhet.in>
2021-02-20 16:41:54 +0530

    Fix default branch name in boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 zip_file=~/Downloads/mac-setup.zip
 extract_dir=~/Downloads/mac-setup
 
-curl -o $zip_file https://github.com/javatarz/mac-setup/archive/master.zip
+curl -o $zip_file https://github.com/javatarz/mac-setup/archive/main.zip
 unzip mac-setup.zip -d $extract_dir
 
 cd $extract_dir
