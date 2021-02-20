9b9796c0730eec537f071ee773cfb933982fde45
Karun Japhet <karun@japhet.in>
2021-02-20 16:40:18 +0530

    Fix downloaded zip path

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@ zip_file=~/Downloads/mac-setup.zip
 extract_dir=~/Downloads/mac-setup
 
 curl -o $zip_file https://github.com/javatarz/mac-setup/archive/master.zip
-unzip master.zip -d $extract_dir
+unzip mac-setup.zip -d $extract_dir
 
 cd $extract_dir
 sh install.sh
