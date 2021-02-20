8f50d2da8a256f97494cee1a092730f8a8c54879
Karun Japhet <karun@japhet.in>
2021-02-21 00:52:08 +0530

    Debug logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,20 @@
 zip_file=~/Downloads/mac-setup.zip
 extract_dir=~/Downloads/mac-setup
+branch=main
 
 set +x
 set -e
 
-curl -o $zip_file -Li https://github.com/javatarz/mac-setup/archive/main.zip
+echo ">> Download latest code from $branch to $zip_file"
+curl -o $zip_file -Li https://github.com/javatarz/mac-setup/archive/$branch.zip
+
+echo ">> Extract $zip_file to $extract_dir"
 unzip $zip_file -d $extract_dir
 
-cd $extract_dir/mac-setup-main
+echo ">> Open $extract_dir/mac-setup-main"
+cd "$extract_dir/mac-setup-main"
+echo ">> Run install"
 sh install.sh
 
+eco ">> Remove $extract_dir and $zip_file"
 rm -rf $extract_dir $zip_file
