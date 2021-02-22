05440eb6928bad8ea22c9cd5bf570829e22ad33a
Karun Japhet <karun@japhet.in>
2021-02-22 12:06:20 +0530

    Fix keyboard speed, this doesn't stick :(

--- osx/track-and-keyboard.sh
+++ osx/track-and-keyboard.sh
@@ -6,5 +6,5 @@ defaults write -g com.apple.trackpad.scaling 3
 
 # Keyboard
 echo ">> Setup keyboard speed parameters"
-defaults write -g InitialKeyRepeat 68
+defaults write -g InitialKeyRepeat 15
 defaults write -g KeyRepeat 2
