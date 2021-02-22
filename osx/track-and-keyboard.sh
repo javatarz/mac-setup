b8ff0f54e277a9610c222eba1801bf75b6238ccd
Karun Japhet <karun@japhet.in>
2021-02-22 12:09:26 +0530

    Add mouse settings

--- osx/track-and-keyboard.sh
+++ osx/track-and-keyboard.sh
@@ -4,6 +4,11 @@
 echo ">> Setup trackpad speed parameters"
 defaults write -g com.apple.trackpad.scaling 3
 
+# Mouse
+echo ">> Setup mouse speed parameters"
+defaults write -g com.apple.mouse.scaling 3
+defaults write -g com.apple.scrollwheel.scaling 1
+
 # Keyboard
 echo ">> Setup keyboard speed parameters"
 defaults write -g InitialKeyRepeat 15
