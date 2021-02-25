555c372355818cbdb1624baed3778b95336082a0
Karun Japhet <karun@japhet.in>
2021-02-25 22:09:10 +0530

    Switching off brew analytics

--- brew/install.sh
+++ brew/install.sh
@@ -5,6 +5,9 @@ echo "> brew/install.sh"
 echo ">> Installing homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo ">> Switching off analytics"
+brew analytics off
+
 echo ">> Remove any apps not on the Brewfile"
 brew bundle cleanup --file=brew/Brewfile --force
 
