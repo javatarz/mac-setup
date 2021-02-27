4a981ff9a4284575884f2d6d6344f71c06cf5d4c
Karun Japhet <karun@japhet.in>
2021-02-27 10:08:32 +0530

    update brew installation command

--- brew/install.sh
+++ brew/install.sh
@@ -3,7 +3,7 @@
 echo
 echo "> brew/install.sh"
 echo ">> Installing homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo ">> Switching off analytics"
 brew analytics off
