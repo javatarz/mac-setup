02114cdc37b00c43573a85fdd0b0c35f97980514
Karun Japhet <karun@japhet.in>
2021-06-30 11:47:37 +0530

    Set docker machine env on request so docker machine does not have to be started up as a service

--- shell/docker.fish
+++ shell/docker.fish
@@ -1,5 +1,26 @@
-eval (docker-machine env default)
-
 function dc
     docker-compose $argv
+end
+
+function dh
+  set command $argv[1]
+
+  if test "$command" = "start"
+    echo ">> Starting docker machine"
+    docker-machine start
+
+    echo ">> Reloading docker machine environment"
+    eval (docker-machine env default)
+
+  else if test "$command" = "shell"
+    echo ">> Reloading docker machine environment"
+    eval (docker-machine env default)
+
+  else if test "$command" = "stop"
+    echo ">> Stopping docker machine"
+    docker-machine stop
+
+  else
+    echo "Unknown command: $command"
+  end
 end
