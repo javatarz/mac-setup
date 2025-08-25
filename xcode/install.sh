9fcadf272e770f5c5170ee35db6c750cf16f94ba
Karun Japhet <karun@japhet.in>
2025-08-25 13:35:08 +0530

    style(xcode): Improve output of install script
    This commit adds echo statements to the xcode install script to provide
    better feedback to the user during the installation process.

--- xcode/install.sh
+++ xcode/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-xcode-select --install
+echo
+echo "> xcode/install.sh"
+
+# Check if Xcode Command Line Tools are already installed
+if xcode-select -p &> /dev/null; then
+  echo ">> Xcode Command Line Tools are already installed."
+else
+  echo ">> Installing Xcode Command Line Tools..."
+  xcode-select --install
+fi
