7abd3df586fda8961d079c15ed5eb942248bc22f
Karun Japhet <karun@japhet.in>
2025-08-25 13:35:22 +0530

    feat(install): Improve robustness of installation script
    This commit improves the main installation script by:
    - Adding `set -e` to exit on error.
    - Ensuring Homebrew is in the PATH.
    - Running the verification script at the end of the installation.

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+set -e # Exit immediately if a command exits with a non-zero status
 
 sh xcode/install.sh
 sh brew/install.sh
+
+echo "> Checking brew is added to path"
+eval "$(brew shellenv)" # Ensure brew is in PATH for subsequent scripts
+if ! echo "$PATH" | grep -q "$(brew --prefix)/bin"; then
+  echo ">> Adding Homebrew to PATH"
+  export PATH="$(brew --prefix)/bin:$PATH"
+fi
+
 sh shell/install.sh
 sh scripts/transfer/transfer.sh
 sh iterm2/install.sh
 sh spectacle/install.sh
 sh alfred/install.sh
 sh osx/install.sh
+sh verify.sh
